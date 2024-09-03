ec4b9b839dccaf44bdcacef640208d15a8dd2db8
Animesh-Chourey <animeshchourey@gmail.com>
2024-09-03 21:54:12 +0100

    New code working on all test cases

--- Array and String/Maximum Average Subarray I.py
+++ Array and String/Maximum Average Subarray I.py
@@ -1,21 +1,19 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        left = 0
-        right = left + k -1
-        max_avg = - pow(10, 6)
+        total = 0
 
-        while right <= len(nums)-1:
-            total = 0
-            temp = left
-            while temp <= right:
-                total = total + nums[temp]
-                temp = temp + 1
-            final = total/k
+        # Sum of the first k elements
+        for i in range(k):  
+            total = total + nums[i]
+        
+        max_avg = total / k #taking the avg of first k elements
 
-            if max_avg < final:
-                max_avg = final
-            
-            left = left + 1
-            right = right + 1
+        for i in range(k, len(nums)):
+            total = total - nums[i-k] # Subtracting the last skipped element after incrementing by 1
+            total = total + nums[i] # Adding the new added element after incrementing 
+
+            new_avg = total / k # Calculating the new avg
+
+            max_avg = max(max_avg, new_avg) # Comparing the previous maximum avg and new avg after incrementing
         
         return max_avg
