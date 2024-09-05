a0d846f3dd7d5c39b76d1555796437698886d261
Animesh-Chourey <animeshchourey@gmail.com>
2024-09-05 14:35:14 +0100

    working soltion to problem 1004

--- Array and String/Max Consecutive Ones III.py
+++ Array and String/Max Consecutive Ones III.py
@@ -1,21 +1,21 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        max_count = 0
-        
-        for i in range(len(nums)):
-            no_flips = 0
-            temp = 0
-            count = 0
+        max_count = 0 
+        left = 0
+        no_flips = 0 # No of zeros encountered and could be flipped
 
-            while no_flips<=k and temp<=len(nums):
-                if nums[i] == 1:
-                    count += 1
-                    temp += 1
-                else:
-                    no_flips += 1
-                    count += 1
-                    temp += 1
+        for right in range(len(nums)):
+            if nums[right] == 0: # If the element is 0
+                no_flips = no_flips + 1
+
+            while no_flips > k: #if flipping becomes greater than the k amount we need to bring it within limit
+                # we will do this by incrementing the left pointer
+                if nums[left] == 0:
+                    no_flips = no_flips - 1
+                
+                left = left + 1
             
+            count = right - left + 1
             max_count = max(max_count, count)
-        
+
         return max_count
